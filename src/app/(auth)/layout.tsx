184bf1079888074df9624fcd4501546d2fbcba6f
Bhushan Shrirame <bhushanshrirame24@gmail.com>
2025-06-24 16:56:18 +0530

    first commit

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import { Children } from "react"
+
+interface AuthLayoutPorps{
+  children: React.ReactNode
+}
+
+const AuthLayout = ({children} :AuthLayoutPorps) => {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <nav className="w-full bg-red-500">Auth navbar</nav>
+      {children}
+      </div>
+  )
+}
+
+export default AuthLayout
