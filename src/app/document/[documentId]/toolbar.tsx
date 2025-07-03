0499f800bc7f181cd910bdc876a9538f0cbb0654
Bhushan Shrirame <bhushanshrirame24@gmail.com>
2025-07-03 20:31:20 +0530

    Font family & heading tools

--- src/app/document/[documentId]/toolbar.tsx
+++ src/app/document/[documentId]/toolbar.tsx
@@ -1,10 +1,128 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
+import{type Level} from "@tiptap/extension-heading"
 import { cn } from "@/lib/utils";
-import { Bold, BoldIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
+import {
+  Bold,
+  BoldIcon,
+  ChevronDownIcon,
+  ItalicIcon,
+  ListTodoIcon,
+  LucideIcon,
+  MessageSquarePlusIcon,
+  PrinterIcon,
+  Redo2Icon,
+  RemoveFormattingIcon,
+  SpellCheckIcon,
+  UnderlineIcon,
+  Undo2Icon,
+} from "lucide-react";
 import { useEditorStore } from "@/store/use-editor-store";
 import { Separator } from "@/components/ui/separator";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const HeadingLevelButton = () => {
+  const { editor } = useEditorStore();
+
+  const heading = [
+    { label: "Normal text", value: 0, fontSize: "16px" },
+    { label: "Headin 1", value: 1, fontSize: "32px" },
+    { label: "Headin 2", value: 2, fontSize: "24px" },
+    { label: "Headin 3", value: 3, fontSize: "20px" },
+    { label: "Headin 4", value: 4, fontSize: "18px" },
+    { label: "Headin 5", value: 5, fontSize: "16px" },
+  ];
+
+  const getCurrentHeading = () => {
+    for(let level = 1; level <= 5; level++){
+      if(editor?.isActive("heading", {level})){
+        return `Heading ${level}`
+      } 
+    }
+    return "Normal text"
+  }
+  return (
+    <DropdownMenu >
+      <DropdownMenuTrigger asChild>
+      <button className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bf-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <span className="truncate">
+            {getCurrentHeading()}
+          </span>
+          <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        { heading.map(({ label, value,fontSize }) => (
+          <button
+            onClick = {() => {
+              if (value ===  0) {
+                editor?.chain().focus().setParagraph().run()
+              }else{
+                editor?.chain().focus().toggleHeading({level:value as Level}).run()
+              }
+            }
+          }
+            key={value}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80 ",
+              (value === 0 && !editor?.isActive("heading")) || editor?.isActive("heading" , {level:value}) && "bg-neutral-200/80"
+            )}
+            style={{ fontSize}}
+          >
+            {label}
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+
+
+
+};
+
+const FontFamilyButton = () => {
+  const { editor } = useEditorStore();
+  const fonts = [
+    { label: "Arial", value: "Arial" },
+    { label: "Times New Roman", value: "Time New Roman" },
+    { label: "Courier New", value: "Courier New" },
+    { label: "Georgia", value: "Georgia" },
+  ];
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bf-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <span className="truncate">
+            {editor?.getAttributes("textStyle").fontFamily || "Arial"}
+          </span>
+          <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {fonts.map(({ label, value }) => (
+          <button
+            onClick={() => editor?.chain().focus().setFontFamily(value).run()}
+            key={value}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80 ",
+              editor?.getAttributes("textStyle").fontFamily === value &&
+                "bg-neutral-200/80"
+            )}
+            style={{ fontFamily: value }}
+          >
+            <span className="text-sm">{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 interface ToolbarButtonProps {
   onClick?: () => void;
@@ -31,7 +149,7 @@ const ToolbarButton = ({
 };
 
 export const Toolbar = () => {
-  const {editor} = useEditorStore();
+  const { editor } = useEditorStore();
   const sections: {
     label: string;
     icon: LucideIcon;
@@ -40,105 +158,98 @@ export const Toolbar = () => {
   }[][] = [
     [
       {
-        label:"Undo",
+        label: "Undo",
         icon: Undo2Icon,
         onClick: () => editor?.chain().focus().undo().run(),
       },
       {
-        label:"Redo",
+        label: "Redo",
         icon: Redo2Icon,
         onClick: () => editor?.chain().focus().redo().run(),
-
       },
       {
-        label:"Print",
+        label: "Print",
         icon: PrinterIcon,
         onClick: () => window.print(),
-
       },
       {
-        label:"Spell Check",
+        label: "Spell Check",
         icon: SpellCheckIcon,
         onClick: () => {
           const current = editor?.view.dom.getAttribute("spellcheck");
-          editor?.view.dom.setAttribute("spellcheck" , current === "false" ? "true" : "false")
-        }
-      }
+          editor?.view.dom.setAttribute(
+            "spellcheck",
+            current === "false" ? "true" : "false"
+          );
+        },
+      },
     ],
     [
-
       {
-        label:"Bold",
+        label: "Bold",
         icon: BoldIcon,
         isActive: editor?.isActive("bold"),
         onClick: () => editor?.chain().focus().toggleBold().run(),
       },
       {
-        label:"Italic",
+        label: "Italic",
         icon: ItalicIcon,
         isActive: editor?.isActive("italic"),
         onClick: () => editor?.chain().focus().toggleItalic().run(),
       },
       {
-        label:"Underline",
+        label: "Underline",
         icon: UnderlineIcon,
         isActive: editor?.isActive("underline"),
         onClick: () => editor?.chain().focus().toggleUnderline().run(),
-      }
+      },
     ],
     [
       {
-        label:"Comment",
+        label: "Comment",
         icon: MessageSquarePlusIcon,
         isActive: false,
         onClick: () => console.log("todo"),
       },
       {
-        label:"List Todo",
+        label: "List Todo",
         icon: ListTodoIcon,
         isActive: editor?.isActive("taskList"),
         onClick: () => editor?.chain().focus().toggleTaskList().run(),
       },
       {
-        label:"Remove Formatting",
+        label: "Remove Formatting",
         icon: RemoveFormattingIcon,
         onClick: () => editor?.chain().focus().unsetAllMarks().run(),
       },
-      
-    ]
+    ],
   ];
   return (
     <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-auto">
       {sections[0].map((item) => (
-        <ToolbarButton key={item.label} {...item}/>
+        <ToolbarButton key={item.label} {...item} />
       ))}
-      <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
-      {/* {Font familr} */}
-      <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
-      {/* {headiing} */}
-      <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      <FontFamilyButton />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      <HeadingLevelButton/>
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {/* {font size} */}
-      <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
-      {
-        sections[1].map((item) => (
-          <ToolbarButton key={item.label} {...item}/>
-        ))
-      }
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {sections[1].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
       {/* {Todo text color} */}
       {/* {highlight color} */}
-      <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {/* {Link} */}
       {/* {Image} */}
       {/* {Align} */}
       {/* {Line Height} */}
       {/* {List} */}
-      {
-        sections[2].map((item) => (
-          <ToolbarButton key={item.label} {...item}/>
-        ))
-      }
-
-
+      {sections[2].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
     </div>
   );
 };
