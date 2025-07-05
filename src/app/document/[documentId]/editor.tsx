c10198f6dbde66643e37ede1425bc470dd494f4e
Bhushan Shrirame <bhushanshrirame24@gmail.com>
2025-07-05 11:53:55 +0530

    Margin ruler component

--- src/app/document/[documentId]/editor.tsx
+++ src/app/document/[documentId]/editor.tsx
@@ -19,6 +19,7 @@ import Link from '@tiptap/extension-link'
 import TextAlign from '@tiptap/extension-text-align'
 import { useEditorStore } from "@/store/use-editor-store";
 import { FontSizeExtension } from "@/extensions/font-size";
+import {Ruler} from "./ruler";
 
 
 
@@ -26,6 +27,7 @@ import { FontSizeExtension } from "@/extensions/font-size";
 export const Editor = () => {
   const {setEditor} = useEditorStore();
   const editor = useEditor({
+    immediatelyRender:false,
     onCreate({editor}){
       setEditor(editor)
     },
@@ -109,6 +111,7 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:bg-white print:overflow-visible">
+      <Ruler/>
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0 ">
         <EditorContent editor={editor} />
       </div>
