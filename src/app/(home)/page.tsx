2fad7e57e4f214c4ca519ac7c1aa60a8d3348932
Bhushan Shrirame <bhushanshrirame24@gmail.com>
2025-07-13 00:12:22 +0530

    database and clerk auth

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,18 @@
+"use client"
+
 import Link from "next/link";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./templates-gallery";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+  const document = useQuery(api.documents.get)
+  if(document === undefined) {
+    return(
+      <p>Loading...</p>
+    )
+  }
   return (
     <div className=" min-h-screen flex flex-col">
       <div className="fixed  top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -10,6 +20,11 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplateGallery/>
+        {
+          document?.map((document) => (
+            <span key = {document._id}>{document.title}</span>
+          ))
+        }
       </div>
     </div>
   );
